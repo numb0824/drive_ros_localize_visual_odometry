9e9db2e6c9ce8e0beb11db64c41be85bbf14ca59
MaslinuPoimal <mykyta.denysov@gmail.com>
2020-10-05 21:07:01 +0200

    Fix for Ubuntu 20

--- include/drive_ros_localize_visual_odometry/simple_visual_odometry.h
+++ include/drive_ros_localize_visual_odometry/simple_visual_odometry.h
@@ -2,7 +2,7 @@
 #define SIMPLE_VISUAL_ODOMETRY_H
 
 #include <ros/ros.h>
-#include <opencv/cv.h>
+#include "opencv2/opencv.hpp"
 
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
